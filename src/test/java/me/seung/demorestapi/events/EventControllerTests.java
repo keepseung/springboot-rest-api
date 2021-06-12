9d91821b13dcb0f52aa572de2bafee46cc8249ea
anseunghyeon <seunghyeon@1milliondance.com>
2021-06-13 00:24:43 +0900

    테스트 케이스에 대한 설명하는 어노테이션 추가함

--- src/test/java/me/seung/demorestapi/events/EventControllerTests.java
+++ src/test/java/me/seung/demorestapi/events/EventControllerTests.java
@@ -1,6 +1,7 @@
 package me.seung.demorestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.seung.demorestapi.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +33,7 @@ public class EventControllerTests {
 //    @MockBean
 //    EventRepository eventRepository;
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         EventDto event = EventDto.builder()
                 .name("Spring")
@@ -62,6 +64,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 받을 수 없는 값을 사용한 경우에 발생하는 이벤트를 생성하는 테스트")
     public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
@@ -89,6 +92,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 비어있는 경우에 에라가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception{
         EventDto eventDto = EventDto.builder().build();
 
@@ -100,6 +104,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 잘못 된 경우에 에라가 발생하는 테스트")
     public void createEvent_Bad_Request_Wrong_Input() throws Exception{
         EventDto eventDto = EventDto.builder()
                 .name("Spring")
