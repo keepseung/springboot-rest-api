d0f01d0118c0d78e5503918bff291979ddb92190
anseunghyeon <seunghyeon@1milliondance.com>
2021-06-13 00:17:19 +0900

    validate wrong value

--- src/test/java/me/seung/demorestapi/events/EventControllerTests.java
+++ src/test/java/me/seung/demorestapi/events/EventControllerTests.java
@@ -98,4 +98,26 @@ public class EventControllerTests {
                 .content(this.objectMapper.writeValueAsString(eventDto)))
                 .andExpect(status().isBadRequest());
     }
+
+    @Test
+    public void createEvent_Bad_Request_Wrong_Input() throws Exception{
+        EventDto eventDto = EventDto.builder()
+                .name("Spring")
+                .description("api desc")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018,11,24,10,10,10))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018,11,22,10,10,10))
+                .beginEventDateTime(LocalDateTime.of(2018,11,24,10,10,10))
+                .endEventDateTime(LocalDateTime.of(2018,11,22,10,10,10))
+                .basePrice(10099)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역")
+                .build();
+
+        // 입력값이 없는 경우 Bad_Request 응답하도록 처리
+        this.mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(this.objectMapper.writeValueAsString(eventDto)))
+                .andExpect(status().isBadRequest());
+    }
 }
