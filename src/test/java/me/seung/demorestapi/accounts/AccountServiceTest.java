79a0054d9c6440d94514be76e7881e3f85a09881
anseunghyeon <seunghyeon@1milliondance.com>
2021-06-13 21:27:06 +0900

    예외 테스트

--- src/test/java/me/seung/demorestapi/accounts/AccountServiceTest.java
+++ src/test/java/me/seung/demorestapi/accounts/AccountServiceTest.java
@@ -1,10 +1,14 @@
 package me.seung.demorestapi.accounts;
 
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 
 import javax.persistence.Table;
@@ -17,6 +21,10 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @ActiveProfiles("test")
 class AccountServiceTest {
+
+    @Rule
+    public ExpectedException expectedException =ExpectedException.none();
+
     @Autowired
     AccountService accountService;
 
@@ -42,4 +50,32 @@ class AccountServiceTest {
 
     }
 
+    @Test
+    public void findByUsernameFail(){
+        String username = "random@email.com";
+
+        // 1. assertThrows로 예외 타입만 확인하기
+        //assertThrows(UsernameNotFoundException.class, ()->accountService.loadUserByUsername(username));
+
+        // 2. try catch로 에러 메세지 확인하기
+        // 장황해지긴 하지만 에러 메세지까지 알 수 있음
+        try {
+            accountService.loadUserByUsername(username);
+            fail("supposed to be failed");
+        }catch (UsernameNotFoundException e){
+            assertThat(e.getMessage()).containsSequence(username);
+        }
+
+        // 3. ExpectedException로 에러를 미리 예측하기
+        // junit 4에 해당하는 코드, 5는 에러남
+        //Expected
+
+        // 에러를 예측하기..
+        /*expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(username));
+
+        // When
+        accountService.loadUserByUsername(username);*/
+
+    }
 }
